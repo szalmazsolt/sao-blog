c30368c1fb73a8c9b54401efe8088422d7ab9820
Zsolt Szalma <zsolt.szalma@gmail.com>
2023-09-12 20:37:01 +0200

    Add blog post CRUD actions

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,6 +1,53 @@
 class BlogPostsController < ApplicationController
 
+  before_action :set_blog_post, except: [:index, :new, :create]
+
   def index
-    @blog_posts = BlogPost.all
+    @blog_posts = BlogPost.order("created_at desc")
   end
+
+  def show
+  end
+
+  def new
+    @blog_post = BlogPost.new
+  end
+
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    if @blog_post.save
+      redirect_to @blog_post
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post.destroy
+    redirect_to blog_posts_url
+  end
+
+  private
+
+    def set_blog_post
+      @blog_post = BlogPost.find(params[:id])
+    end
+
+    def blog_post_params
+      params
+        .require(:blog_post)
+        .permit(:title, :body)
+    end
 end
