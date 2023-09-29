8051400ea261607fe1996cf9db06dc65d5ac6f45
Zsolt Szalma <zsolt.szalma@gmail.com>
2023-09-29 20:58:42 +0200

    Add tests for blog post status methods

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -4,4 +4,41 @@ class BlogPostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "draft? should return true for draft post" do
+    # the blog_posts method is given us by the blog post fixture
+    assert blog_posts(:draft).draft?
+  end
+
+  test "draft? should return false for published post" do
+     refute blog_posts(:published).draft?
+  end
+
+  test "draft? should return false for scheduled post" do
+    refute blog_posts(:scheduled).draft?
+  end
+
+  test "published? should return true for published post" do
+    assert blog_posts(:published).published?
+  end
+
+  test "published? should return false for draft post" do
+    refute blog_posts(:draft).published?
+  end
+
+  test "published? should return false for scheduled post" do
+    refute blog_posts(:scheduled).published?
+  end
+
+  test "scheduled? should return true for scheduled post" do
+   assert blog_posts(:scheduled).scheduled?
+  end
+
+  test "scheduled? should return false for draft post" do
+    refute blog_posts(:draft).scheduled?
+  end
+
+  test "scheduled? should return false for published post" do
+    refute blog_posts(:published).scheduled?
+  end
 end
